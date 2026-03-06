5f1461feabf8c54b935792b2b5da0352fc5accb3
Eric Oliveros <macot_14@yahoo.com>
2026-03-06 21:08:59 +0100

    rollback previous commit

--- frontend/lib/supabase.ts
+++ frontend/lib/supabase.ts
@@ -4,7 +4,7 @@ import { createClient } from '@supabase/supabase-js';
 import { AppState, Platform } from 'react-native';
 
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL || 'https://bruzgztsltjtzwkkehif.supabase.co';
-const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJydXpnenRzbHRqdHp3a2tlaGlmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk3NTg0NzMsImV4cCI6MjA4NTMzNDQ3M30.bzHCtZpAyTI3QXCyvbVFn9Tb-Cp0iI2Qse9zJhHB_ow';
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || '';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
