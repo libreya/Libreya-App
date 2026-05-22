689fbadfbe5a9aecac8ad263adc31d5e9436c9c4
Eric Oliveros <macot_14@yahoo.com>
2026-05-22 23:38:45 +0200

    test

--- frontend/lib/supabase.ts
+++ frontend/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://bruzgztsltjtzwkkehif.supabase.co';
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJydXpnenRzbHRqdHp3a2tlaGlmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk3NTg0NzMsImV4cCI6MjA4NTMzNDQ3M30.bzHCtZpAyTI3QXCyvbVFn9Tb-Cp0iI2Qse9zJhHB_ow';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
